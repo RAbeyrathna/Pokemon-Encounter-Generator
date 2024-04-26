be8eb43812230b6841aad0fb304269b50ab9fd0e
RAbeyrathna <rahal@res-network.com>
2024-04-27 09:59:56 +1000

    feat: Add type 1/2 prefix to display

--- src/pokemonAPI.js
+++ src/pokemonAPI.js
@@ -25,11 +25,11 @@ async function putDataOnPage(dataToDisplay){
     let type1Display = document.querySelector(".pokemonType1");
     let type2Display = document.querySelector(".pokemonType2");
 
-    type1Display.textContent = dataToDisplay.types[0].type.name;
+    type1Display.textContent = "Type 1: " + dataToDisplay.types[0].type.name;
 
     if (dataToDisplay.types[1]){
         // If second element exists, set second type display
-        type2Display.textContent = dataToDisplay.types[1].type.name;
+        type2Display.textContent = "Type 2: " + dataToDisplay.types[1].type.name;
     }
     else{
         type2Display.textContent = "";
